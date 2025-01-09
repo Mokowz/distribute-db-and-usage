132c2b805139525bd7645c82c90b6b54160e5c27
Ronny Mokaya <ronniemokaya30@gmail.com>
2025-01-09 11:23:28 +0300

    Add the sql files with populated data

---
+++ courses.sql
@@ -0,0 +1,8 @@
+-- SQL script to insert data into the semesters table
+INSERT INTO semesters (course_ID, course_name) VALUES
+('CS001', 'Computer Science'),
+('IT001', 'Information Technology'),
+('SE001', 'Software Engineering'),
+('BCT2101', 'Business Computing'),
+('PMI2203', 'Project Management'),
+('AFT2202', 'Accounting and Finance');
---
+++ units.sql
@@ -0,0 +1,8 @@
+-- SQL script to insert data into the Unit table
+INSERT INTO Unit (unit_ID, unit_code, unit_name, course_ID) VALUES
+('UNIT001', 'ICS2101', 'Introduction to Computer Science', 'CS001'),
+('UNIT002', 'ICS2202', 'Data Structures and Algorithms', 'CS001'),
+('UNIT003', 'BCT2103', 'Introduction to IT', 'IT001'),
+('UNIT004', 'BCT2201', 'Networking Basics', 'IT001'),
+('UNIT005', 'ICS2301', 'Software Design', 'SE001'),
+('UNIT006', 'ICS2302', 'Software Testing', 'SE001');
